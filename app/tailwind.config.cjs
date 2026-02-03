ee8de792b26abb08d556ff62b687ee36a0816874
Melvin Loos <loos@glr.nl>
2026-02-03 17:17:52 +0100

    chore(tailwind): add Tailwind and PostCSS config

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lime: '#8fe507'
+      }
+    }
+  },
+  plugins: []
+}
